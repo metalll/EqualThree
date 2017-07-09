105e842857114a3ef1b0557d6b8a89210e05498a
metalll <metall112@bk.ru>
2017-07-09 16:33:24 +0300

    Initial Commit

---
+++ EqualThree/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  EqualThree
+//
+//  Created by NSD on 09.07.17.
+//  Copyright © 2017 NSD NULL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
