8e7475aa78e2a8164a70827884181b28d90c363e
metalll <metall112@bk.ru>
2017-08-03 04:01:46 +0300

    fix UI hint

--- EqualThree/NSDGameFieldViewController.m
+++ EqualThree/NSDGameFieldViewController.m
@@ -105,8 +105,8 @@ NSUInteger const NSDCostItem = 10;
     
     
     
-    self.horizontalItemsCount = 9;
-    self.verticalItemsCount = 9;
+    self.horizontalItemsCount = 10;
+    self.verticalItemsCount = 10;
     self.itemTypesCount = NSDGameItemTypesCount;
     
     self.itemSize = CGSizeMake(self.gameItemsView.frame.size.width / (CGFloat) self.horizontalItemsCount,
